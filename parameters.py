5826b9f3d2a194e82bd4d9fd56a1d91ee4dd1c30
Felipe Nonato <felipe_junior_jp@outlook.com.br>
2026-09-15 01:01:08 +0900

    experiment commit

--- parameters.py
+++ parameters.py
@@ -39,11 +39,11 @@ PARAMETERS = {
     "selectionFunc": individual_selection_v0,
     "parentSelec": select_parent2_random,
     "tolerance": 0.5,
-    "number_generation":5,
+    "number_generation":300,
     "max_memory":500,
     "cross_over":0.5,
     "mutation":0.1,
-    "update_interval":2
+    "update_interval":50
 }
 PARAMETERS["population_size"] = sum(len(line) for line in PARAMETERS["grid"])
 
